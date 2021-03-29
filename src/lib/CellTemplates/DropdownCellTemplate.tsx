8bd75f4a62a45eb679a8732d379752c13839eeb1
patryk0493 <patryk0493@gmail.com>
2021-03-29 23:11:27 +0200

    a draft

--- src/lib/CellTemplates/DropdownCellTemplate.tsx
+++ src/lib/CellTemplates/DropdownCellTemplate.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+/* eslint-disable */
 
 // NOTE: all modules imported below may be imported from '@silevis/reactgrid'
 import { getCellProperty } from '../Functions/getCellProperty';
@@ -81,89 +82,173 @@ export class DropdownCellTemplate implements CellTemplate<DropdownCell> {
         onCellChanged: (cell: Compatible<DropdownCell>, commit: boolean) => void
     ): React.ReactNode {
         // TODO create custom hook - useDropdown
+        const { inputValue: iv, values, isOpen, isDisabled, selectedValue } = cell;
 
         //eslint-disable-next-line
         const selectRef = React.useRef<any>(null);
         //eslint-disable-next-line
-        const [inputValue, setInputValue] = React.useState<string | undefined>(cell.inputValue);
+        const [inputValue, setInputValue] = React.useState<string | undefined>(iv);
         //eslint-disable-next-line
         React.useEffect(() => {
-            if (cell.isOpen && selectRef.current) {
+            if (isOpen && selectRef.current) {
                 selectRef.current.focus();
-                setInputValue(cell.inputValue);
+                setInputValue(iv);
             }
-        }, [cell.isOpen, cell.inputValue]);
+        }, [isOpen, iv]);
+
+        //eslint-disable-next-line
+        const call = React.useCallback(() => {
+            onCellChanged(this.getCompatibleCell({ ...cell, isOpen: true }), true)
+        }, [cell, onCellChanged])
+
+        //eslint-disable-next-line
+        const onChange = React.useCallback(() => {
+            onCellChanged(this.getCompatibleCell({ ...cell, isOpen: !isOpen, inputValue: undefined }), true)
+        }, [onCellChanged, isOpen, cell]);
+
+        //eslint-disable-next-line
+        const onInputChange = React.useCallback(e => setInputValue(e), []);
 
         return (
             <div
                 style={{ width: '100%' }}
-                onPointerDown={e => onCellChanged(this.getCompatibleCell({ ...cell, isOpen: true }), true)}
+                onPointerDown={call}
             >
-                <Select
-                    {...(cell.inputValue && {
-                        inputValue,
-                        defaultInputValue: inputValue,
-                        onInputChange: e => setInputValue(e),
-                    })}
-                    isSearchable={true}
-                    ref={selectRef}
-                    {...(cell.isOpen !== undefined && { menuIsOpen: cell.isOpen })}
-                    onMenuClose={() => onCellChanged(this.getCompatibleCell({ ...cell, isOpen: !cell.isOpen, inputValue: undefined }), true)}
-                    onMenuOpen={() => onCellChanged(this.getCompatibleCell({ ...cell, isOpen: true }), true)}
-                    onChange={(e) => onCellChanged(this.getCompatibleCell({ ...cell, selectedValue: (e as OptionType).value, isOpen: false, inputValue: undefined }), true)}
-                    blurInputOnSelect={true}
-                    defaultValue={cell.values.find(val => val.value === cell.selectedValue)}
-                    isDisabled={cell.isDisabled}
-                    options={cell.values}
-                    onKeyDown={e => e.stopPropagation()}
-                    components={{
-                        Option: CustomOption,
-                        Menu: CustomMenu,
-                    }}
-                    styles={{
-                        container: (provided) => ({
-                            ...provided,
-                            width: '100%',
-                            height: '100%',
-                        }),
-                        control: (provided) => ({
-                            ...provided,
-                            border: 'none',
-                            borderColor: 'transparent',
-                            minHeight: '25px',
-                            background: 'transparent',
-                            boxShadow: 'none',
-                        }),
-                        indicatorsContainer: (provided) => ({
-                            ...provided,
-                            paddingTop: '0px',
-                        }),
-                        dropdownIndicator: (provided) => ({
-                            ...provided,
-                            padding: '0px 4px',
-                        }),
-                        singleValue: (provided) => ({
-                            ...provided,
-                            color: 'inherit'
-                        }),
-                        indicatorSeparator: (provided) => ({
-                            ...provided,
-                            marginTop: '4px',
-                            marginBottom: '4px',
-                        }),
-                        input: (provided) => ({
-                            ...provided,
-                            padding: 0,
-                        }),
-                        valueContainer: (provided) => ({
-                            ...provided,
-                            padding: '0 8px',
-                        }),
-                    }}
-                />
-            </div >
+                <A iv={iv} inputValue={inputValue} selectRef={selectRef} isOpen={isOpen} onChange={onChange} call={call}
+                    values={values} selectedValue={selectedValue} isDisabled={isDisabled} onInputChange={onInputChange}></A>
+            </div>
         );
     }
+}
+
+const A = React.memo<CProps>(({ iv, inputValue, selectRef, isOpen, onChange, call, values, selectedValue, isDisabled, onInputChange }) => {
+    return <C
+        iv={iv} inputValue={inputValue} selectRef={selectRef} isOpen={isOpen} onChange={onChange} call={call}
+        values={values} selectedValue={selectedValue} isDisabled={isDisabled} onInputChange={onInputChange}
+    />
+}, (prev, next) => {
+    // return false;
+    return prev.isOpen === next.isOpen
+        // || prev.call === next.call
+        // || prev.inputValue === next.inputValue
+        // || prev.isDisabled === next.isDisabled
+        // || prev.iv === next.iv
+        // || prev.onChange === next.onChange
+        // || prev.onInputChange === next.onInputChange
+        // || prev.selectRef === next.selectRef
+        || prev.selectedValue === next.selectedValue
+    // || prev.values === next.values;
+});
+
+interface CProps {
+    iv: string | undefined;
+    inputValue: string | undefined;
+    selectRef: React.MutableRefObject<any>;
+    isOpen: boolean | undefined;
+    onChange: (...ars: any) => any,
+    call: (...ars: any) => any,
+    values: any[],
+    selectedValue: string | undefined,
+    isDisabled: boolean | undefined;
+    onInputChange: (...ars: any) => any,
+}
+
+const C: React.FC<CProps> = ({ iv, inputValue, selectRef, isOpen, onChange, call, values, selectedValue, isDisabled, onInputChange }) => {
+    //eslint-disable-next-line
+    const styles = useStyles();
+
+    return <Select
+        {...(iv && {
+            inputValue,
+            defaultInputValue: inputValue,
+            onInputChange,
+        })}
+        isSearchable={true}
+        ref={selectRef}
+        {...(isOpen !== undefined && { menuIsOpen: isOpen })}
+        onMenuClose={onChange}
+        onMenuOpen={call}
+        onChange={onChange}
+        blurInputOnSelect={true}
+        defaultValue={values.find(val => val.value === selectedValue)}
+        isDisabled={isDisabled}
+        options={values}
+        onKeyDown={e => e.stopPropagation()}
+        components={{
+            Option: CustomOption,
+            Menu: CustomMenu,
+        }}
+        styles={styles}
+    />
+}
+
+const useStyles = () => {
+
+    //eslint-disable-next-line
+    const container = React.useCallback((provided) => ({
+        ...provided,
+        width: '100%',
+        height: '100%',
+    }), []);
+
+    //eslint-disable-next-line
+    const control = React.useCallback((provided) => ({
+        ...provided,
+        border: 'none',
+        borderColor: 'transparent',
+        minHeight: '25px',
+        background: 'transparent',
+        boxShadow: 'none',
+    }), []);
+
+    //eslint-disable-next-line
+    const indicatorsContainer = React.useCallback((provided) => ({
+        ...provided,
+        paddingTop: '0px',
+    }), []);
+
+    //eslint-disable-next-line
+    const dropdownIndicator = React.useCallback((provided) => ({
+        ...provided,
+        padding: '0px 4px',
+    }), []);
+
+    //eslint-disable-next-line
+    const input = React.useCallback((provided) => ({
+        ...provided,
+        padding: 0,
+    }), []);
+
+    //eslint-disable-next-line
+    const valueContainer = React.useCallback((provided) => ({
+        ...provided,
+        padding: '0 8px',
+    }), []);
+
+    //eslint-disable-next-line
+    const singleValue = React.useCallback((provided) => ({
+        ...provided,
+        color: 'inherit'
+    }), []);
+
+    //eslint-disable-next-line
+    const indicatorSeparator = React.useCallback((provided) => ({
+        ...provided,
+        marginTop: '4px',
+        marginBottom: '4px',
+    }), []);
+
+    return {
+        container,
+        control,
+        indicatorsContainer,
+        dropdownIndicator,
+        singleValue,
+        indicatorSeparator,
+        input,
+        valueContainer,
+    }
+
 }
 
 const CustomOption: React.FC<OptionProps<OptionType, false>> = ({ innerProps, label, isSelected, isFocused }) => (
